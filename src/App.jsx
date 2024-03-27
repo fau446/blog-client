b824b4898a8ec89409e6662be731e6fecfd3807f
Franklin <Franklinau2@gmail.com>
2024-03-27 09:28:06 -0700

    Initial vite setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // fetch all published posts
+  return (
+    <>
+      <h1>Blog</h1>
+    </>
+  );
+}
+
+export default App;
