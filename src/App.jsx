2e96869a616e6f79ab84dbdb2146c44ea5085f62
Franklin <Franklinau2@gmail.com>
2024-03-31 09:10:02 -0700

    Add: Published posts displayed on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Post from "./components/Post";
 
 function App() {
-  // fetch all published posts
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch("http://localhost:3000/posts");
+        const jsonData = await response.json();
+        setPosts(jsonData.posts);
+        console.log(jsonData.posts);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <h1>Blog</h1>
+      {posts.map((post, index) => (
+        <div>
+          <Post key={index} title={post.title} date={post.date} />
+        </div>
+      ))}
     </>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post({ date, title }) {
+  return (
+    <div>
+      <p>{date}</p>
+      <h2>{title}</h2>
+    </div>
+  );
+}
+
+export default Post;
