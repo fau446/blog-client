b74f6a98b0947f01a0fa800c091ee7029b1453a6
Franklin <Franklinau2@gmail.com>
2024-04-01 15:02:49 -0700

    Add: Commenting Feature

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -5,18 +5,42 @@ import { useParams } from "react-router-dom";
 function PostDetail() {
   const [postDetail, setPostDetail] = useState({});
   const { id } = useParams();
+  const [formData, setFormData] = useState({ name: "", message: "" });
+
+  async function fetchPostDetails() {
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${id}`);
+      const jsonData = await response.json();
+      setPostDetail(jsonData.post);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleInputChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      await response.json();
+      setFormData({ name: "", message: "" });
+      await fetchPostDetails();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   useEffect(() => {
-    async function fetchPostDetails() {
-      try {
-        const response = await fetch(`http://localhost:3000/posts/${id}`);
-        const jsonData = await response.json();
-        setPostDetail(jsonData.post);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
     fetchPostDetails();
   }, []);
 
@@ -34,6 +58,24 @@ function PostDetail() {
             <p>{comment.message}</p>
           </div>
         ))}
+      <h2>Write a comment!:</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="message">Message:</label>
+        <input
+          type="text"
+          name="message"
+          value={formData.message}
+          onChange={handleInputChange}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
